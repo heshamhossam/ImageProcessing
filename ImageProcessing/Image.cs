ec87e9e4923334ef08463dfa61fc93104d527372
Kit <eslam_fcih@hotmail.com>
2014-08-27 08:25:45 -0600

    add Image Class Constructor and Compose Method
    add Image Class Constructor and Compose Method

--- ImageProcessing/Image.cs
+++ ImageProcessing/Image.cs
@@ -12,7 +12,6 @@ namespace ImageProcessing
         private string _imagePath;
         private Bitmap _bitmapImage;
 
-
         protected Color[,] _imageMatrix;
         public Color[,] ImageMatrix
         {
@@ -27,8 +26,9 @@ namespace ImageProcessing
         {
             // TODO: Complete member initialization and constructs the bitmap image as well as the image matrix
             this._imagePath = imagePath;
-            
-            
+            this._bitmapImage = new Bitmap(imagePath);
+           this._imageMatrix =  GetImageMatrix(this._bitmapImage);/////////////////Construct The Image Matrix
+
         }
 
         public Image()
@@ -49,9 +49,34 @@ namespace ImageProcessing
         /// Compose the current image from the list of images given
         /// </summary>
         /// <param name="_images"></param>
-        public void compose(List<Image> _images)
+        public void compose(List<Bitmap> _images)
         {
-            throw new NotImplementedException();
+            foreach(Bitmap bit in _images)
+            {
+                Color[,] imagematrixtemp = GetImageMatrix(bit);
+                for (int i = 0; i < imagematrixtemp.GetLength(0); i++)
+                {
+                    for (int j = 0; j < imagematrixtemp.GetLength(1); j++)
+                    {
+                        this._bitmapImage.SetPixel(this._bitmapImage.Width+i, this._bitmapImage.Height + j, imagematrixtemp[imagematrixtemp.GetLength(0) - i - 1, imagematrixtemp.GetLength(1) - j - 1]);
+                    }
+                }
+            }
+        }
+        private Color[,] GetImageMatrix(Bitmap bitmap)
+        {
+
+            Color[,] imagematrix = new Color[bitmap.Width, bitmap.Height];
+
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    imagematrix[x, y] = bitmap.GetPixel(x, y);
+                }
+            }
+
+            return imagematrix;
         }
     }
 }
